4ba1a7c2d5528fca6a57122974b103f5fa7bf203
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2022-12-26 23:13:02 -0600

    Started adding post and get functions

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+
+app.use(express.json());
+/*
 
 app.get('/', (request, response) => {
     response.status(200)
@@ -11,6 +15,25 @@ app.get('/', (request, response) => {
 app.post('/', (req, res) => {
     res.send('You can post to this endpoint...');
 });
+*/
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+app.post('/app/v1/tours', (req, res) => {
+    console.log(req.body);
+    res.send('done');
+})
 
 const port = 3000;
 
