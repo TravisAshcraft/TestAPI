5a21fe445b27e34d320cd7b2e68430bf667f6d5f
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2022-12-27 23:13:51 -0600

    Added the rest of CRUD
    Added in patch, delete, and finding item by id.

--- app.js
+++ app.js
@@ -30,10 +30,76 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
-app.post('/app/v1/tours', (req, res) => {
-    console.log(req.body);
-    res.send('done');
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1;
+    //loop through the array and find the correct one
+    const tour = tours.find(el => el.id === id);
+
+    if(!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours[tours.length - 1].id +1;
+    const newTour = Object.assign({id: newID}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours) , err =>{
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: newTour
+            }
+        });
+
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    });
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+    res.status(204).json({
+        status: 'Success',
+        data: null
+    });
+})
+
 
 const port = 3000;
 
