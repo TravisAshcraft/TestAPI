e86fc8054bade31525d7ea8315d6608f51fe6495
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2023-01-01 08:04:54 -0600

    Password Validation and Encryption
    Added in basic validator when creating new user so that the pass and the passConfirm have to match in order to create/modify user. Added in encryption to the pass with bcryptjs to keep pass encrypted on the DB.

--- Modules/userModule.js
+++ Modules/userModule.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //describes what is required to create a colleciton on mongo
 const userSchema = new mongoose.Schema({
@@ -25,9 +26,28 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            //this only works on CREATE and SAVE!!!
+            validator: function(el){
+                return el === this.password;
+            },
+            message: 'Passowrds are not the same!'
+        }
 
     }
+});
+
+userSchema.pre('save', async function(next) {
+    //We only encrypt password on create and save
+    if(!this.isModified('password')) return next();
+
+    //Hash power set to 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //We set the confirm password to undefined after Creat or Save to get rid of it we only need one 
+    this.passwordConfirm = undefined;
+    next();
 })
 
 const User = mongoose.model('User', userSchema);
