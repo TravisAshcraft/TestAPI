519af43d86833a0835d336a747364e45afa67383
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2023-01-01 08:44:18 -0600

    Added JWT auth
    In this push I have added the JWT package from npm. We have our JWT token being given to the users when they are creating their account I will be working on the logging in system to be able to log the users in.

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../Modules/userModule');
 const catchAsync = require('./../Utils/catchAsync');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRES_IN
+    } );
 
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user: newUser
         }
